ddf8b751ce56b40739a5bea8c5f5eaeba19a601e
Michael Tighe <michaelt@zumasys.com>
2019-09-17 15:29:39 -0700

    Add logging and exception handling to WishSearchAsync

--- NzbWishlist.Azure/Functions/SearchFunctions.cs
+++ NzbWishlist.Azure/Functions/SearchFunctions.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
 using NzbWishlist.Azure.Models;
 using NzbWishlist.Core.Models;
 using NzbWishlist.Core.Services;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace NzbWishlist.Azure.Functions
@@ -17,16 +20,26 @@ namespace NzbWishlist.Azure.Functions
         }
 
         [FunctionName(Constants.WishSearchActivity)]
-        public async Task<IEnumerable<WishResult>> WishSearchAsync([ActivityTrigger] SearchWishContext context)
+        public async Task<IEnumerable<WishResult>> WishSearchAsync(
+            [ActivityTrigger] SearchWishContext context,
+            ILogger log)
         {
-            var results = await _client.SearchAsync(context.Provider, context.Wish);
-
-            foreach(var result in results)
+            try
             {
-                result.BelongsTo(context.Wish);
-            }
+                var results = await _client.SearchAsync(context.Provider, context.Wish);
 
-            return results;
+                foreach (var result in results)
+                {
+                    result.BelongsTo(context.Wish);
+                }
+
+                return results;
+            }
+            catch (Exception ex)
+            {
+                log.LogCritical(ex, "NewznabClient caused an exception");
+                return Enumerable.Empty<WishResult>();
+            }
         }
     }
 }
--- NzbWishlist.Tests/Functions/SearchFunctionsTests.cs
+++ NzbWishlist.Tests/Functions/SearchFunctionsTests.cs
@@ -3,7 +3,10 @@ using NzbWishlist.Azure.Functions;
 using NzbWishlist.Azure.Models;
 using NzbWishlist.Core.Models;
 using NzbWishlist.Core.Services;
+using NzbWishlist.Tests.Fixtures;
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace NzbWishlist.Tests.Functions
@@ -13,6 +16,7 @@ namespace NzbWishlist.Tests.Functions
     {
         private readonly SearchFunctions _function;
         private readonly Mock<INewznabClient> _client = new Mock<INewznabClient>(MockBehavior.Strict);
+        private readonly MockLogger _log = new MockLogger();
 
         public SearchFunctionsTests()
         {
@@ -20,7 +24,7 @@ namespace NzbWishlist.Tests.Functions
         }
 
         [Fact]
-        public async void WishSearchAsync_Invokes_The_NewznabClient()
+        public async Task WishSearchAsync_Invokes_The_NewznabClient()
         {
             var ctx = new SearchWishContext
             {
@@ -30,13 +34,13 @@ namespace NzbWishlist.Tests.Functions
             _client.Setup(c => c.SearchAsync(ctx.Provider, ctx.Wish))
                    .ReturnsAsync(Enumerable.Empty<WishResult>());
 
-            await _function.WishSearchAsync(ctx);
+            await _function.WishSearchAsync(ctx, _log.Object);
 
             _client.Verify(c => c.SearchAsync(ctx.Provider, ctx.Wish), Times.Once());
         }
 
         [Fact]
-        public async void WishSearchAsync_Associates_Wish_Results_To_Their_Wish()
+        public async Task WishSearchAsync_Associates_Wish_Results_To_Their_Wish()
         {
             var ctx = new SearchWishContext
             {
@@ -46,7 +50,7 @@ namespace NzbWishlist.Tests.Functions
             _client.Setup(c => c.SearchAsync(ctx.Provider, ctx.Wish))
                    .ReturnsAsync(new[] { new WishResult { } });
 
-            var results = await _function.WishSearchAsync(ctx);
+            var results = await _function.WishSearchAsync(ctx, _log.Object);
 
             _client.Verify(c => c.SearchAsync(ctx.Provider, ctx.Wish), Times.Once());
             Assert.All(results, r =>
@@ -54,6 +58,22 @@ namespace NzbWishlist.Tests.Functions
                 Assert.Equal("A Wish", r.WishName);
                 Assert.NotNull(r.RowKey);
             });
+        }
+
+        [Fact]
+        public async Task WishSearchAsync_Logs_Exceptions()
+        {
+            var ctx = new SearchWishContext
+            {
+                Provider = new Provider(),
+                Wish = new Wish()
+            };
+            _client.Setup(c => c.SearchAsync(ctx.Provider, ctx.Wish)).ThrowsAsync(new Exception("uh oh!"));
+
+            var results = await _function.WishSearchAsync(ctx, _log.Object);
+
+            _log.VerifyLoggedException("NewznabClient caused an exception");
+            Assert.Empty(results);
         }
     }
 }
